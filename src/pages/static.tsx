93d857b93ad03aa5ca01558e11f1bb086b5da13f
Cauan Diniz <cauanrodiniz@gmail.com>
2024-02-20 21:35:20 -0300

    Creating APIs in Next.JS

--- src/pages/static.tsx
+++ src/pages/static.tsx
@@ -1,66 +1,52 @@
-import { GetServerSideProps, GetStaticProps, NextPage } from "next"
-import { ReactNode, useEffect, useState } from "react"
-import { Col, Container, Row } from "reactstrap"
+import { GetServerSideProps, GetStaticProps, NextPage } from "next";
+import { ReactNode, useEffect, useState } from "react";
+import { Col, Container, Row } from "reactstrap";
 
 interface ApiResponse {
-  name: string
-  timestamp: Date
+  name: string;
+  timestamp: Date;
 }
 
 export const getStaticProps: GetStaticProps = async () => {
-  const staticData = await fetch(`${process.env.NEXT_PUBLIC_APIURL}/api/hello`).then(res => res.json())
-  
+  const staticData = await fetch(`${process.env.NEXT_PUBLIC_APIURL}/api/hello`).then((res) => res.json());
+
   return {
     props: {
-      staticData
+      staticData,
     },
-    revalidate: 10
-  }
-}
+    revalidate: 10,
+  };
+};
 
-const Static: NextPage = (props: {
-    children?: ReactNode
-    staticData?: ApiResponse
-}) => {
-  const [clientSideData, setClientSideData] = useState<ApiResponse>()
+const Static: NextPage = (props: { children?: ReactNode; staticData?: ApiResponse }) => {
+  const [clientSideData, setClientSideData] = useState<ApiResponse>();
 
   useEffect(() => {
-      fetchData()
-  }, [])
+    fetchData();
+  }, []);
 
   const fetchData = async () => {
-      const data = await fetch("/api/hello").then(res => res.json())
-      
-      setClientSideData(data) 
-  }
+    const data = await fetch("/api/hello").then((res) => res.json());
+
+    setClientSideData(data);
+  };
   return (
     <Container tag="main">
-    <h1 className="my-5">
-      Como funcionam as renderizações do Next.js
-    </h1>
+      <h1 className="my-5">Como funcionam as renderizações do Next.js</h1>
 
-    <Row>
-      <Col>
-        <h3>
-          Gerado estaticamente durante o build:
-        </h3>
-        <h2>
-          {props.staticData?.timestamp.toString()}
-        </h2>
-      </Col>
+      <Row>
+        <Col>
+          <h3>Gerado estaticamente durante o build:</h3>
+          <h2>{props.staticData?.timestamp.toString()}</h2>
+        </Col>
+        <Col>
+          <h3>Gerado no cliente:</h3>
 
-      <Col>
-        <h3>
-          Gerado no cliente:
-        </h3>
+          <h2>{clientSideData?.timestamp.toString()}</h2>
+        </Col>
+      </Row>
+    </Container>
+  );
+};
 
-        <h2>
-          {clientSideData?.timestamp.toString()}
-        </h2>
-      </Col>
-    </Row>
-  </Container>
-  )
-}
-
-export default Static
+export default Static;
